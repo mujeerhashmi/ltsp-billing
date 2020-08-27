e26707ff66d753943d79f7690ac2eb3004013ae8
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2020-08-27 22:56:29 +0530

    Fixtures: Print format fixtures
    Signed-off-by: Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>

--- cimec_infotech/hooks.py
+++ cimec_infotech/hooks.py
@@ -11,6 +11,21 @@ app_color = "grey"
 app_email = "info@4csolutions.in"
 app_license = "MIT"
 
+fixtures = [
+    {
+		"doctype": "Print Format",
+		"filters": [
+			[
+				"name",
+				"in",
+				[
+					"Cimec GST Invoice",
+                                        "Cimec Installation Note"
+				]
+			]
+		]
+	}
+]
 # Includes in <head>
 # ------------------
 
